4d8dd1f6c45902dae75306e2c2a2460a60ea9cd3
bobmnc <bobomnc2b@gmail.com>
2024-02-09 16:15:57 +0100

    take outputs first token embedding

--- visualisation/embed.py
+++ visualisation/embed.py
@@ -13,11 +13,17 @@ def embed_dataset(data_dir : str,embedding_model : str = 'distilbert-base-uncase
     for category in tqdm(categories) :
         ### to do : batched processing
         for text_file in os.listdir(join(data_dir,category)):
-            text = torch.load(text_file)
+            text = torch.load(join(data_dir,
+                                   category,
+                                   text_file))
+            print(text.shape)
             with torch.no_grad():
                 embedding = model(text)
-            list_embeds.append(embedding.numpy())
+            list_embeds.append(embedding.last_hidden_state[:,0].numpy())
             labels.append(category)
     return list_embeds,labels
+
+if __name__=='__main__':
+    embed_dataset('../data/tokenized_data')
 
 
