490d77ef80cce02b3e5595f0b16df2f5086f1989
bobmnc <bobomnc2b@gmail.com>
2024-02-09 14:21:23 +0100

    create a script to get urls from category name

---
+++ scrapping/get_urls_interest.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def get_urls(categories : list[str]):
+    all_article_urls = []
+
+    # Iterate over each category
+    for category in categories:
+        api_url = 'https://en.wikipedia.org/w/api.php'
+        # Define parameters for the API request
+        params = {
+            'action': 'query',
+            'list': 'categorymembers',
+            'cmtitle': category,
+            'cmlimit': 500,  # Number of articles to retrieve per category
+            'format': 'json'
+        }
+
+        # Send a GET request to the Wikipedia API
+        response = requests.get(api_url, params=params)
+        data = response.json()
+
+        # Extract article titles from the response
+        article_titles = [page['title'] for page in data['query']['categorymembers']]
+
+        # Construct URLs from the article titles
+        base_url = 'https://en.wikipedia.org/wiki/'
+        article_urls = [base_url + title.replace(' ', '_') for title in article_titles]
+
+        all_article_urls.extend(article_urls)
+
+        return all_article_urls
+
+if __name__=='__main__':
+    urls = get_urls(['Category:Machine learning'])
+    print(urls[:10])
